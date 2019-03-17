a6835a09c055844b69ab440a168632762a13424d
MSI\11346 <1134685565@qq.com>
2019-03-17 11:47:32 +0800

    no message

---
+++ 2.23.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=0, b=0, c=0, d=0, e=0;
+    cin >> a >> b >> c >> d >> e;
+
+    if ( a > b )
+    if ( a > c )
+    if ( a > d )
+    if ( a > e )
+    cout << a << endl;
+
+    if ( b > a )
+    if ( b > c )
+    if ( b > d )
+    if ( b > e )
+    cout << b << endl;
+
+    if ( c > a )
+    if ( c > b )
+    if ( c > d )
+    if ( c > e )
+    cout << c << endl;;
+
+    if ( d > a )
+    if ( d > c )
+    if ( d > b )
+    if ( d > e )
+    cout << d << endl;
+
+    if ( e > a )
+    if ( e > c )
+    if ( e > d )
+    if ( e > b )
+    cout << e << endl;
+
+    if ( a < b )
+    if ( a < c )
+    if ( a < d )
+    if ( a < e )
+    cout << a << endl;
+
+    if ( b < a )
+    if ( b < c )
+    if ( b < d )
+    if ( b < e )
+    cout << b << endl;
+
+    if ( c < b )
+    if ( c < a )
+    if ( c < d )
+    if ( c < e )
+    cout << c << endl;
+
+    if ( d < b )
+    if ( d < c )
+    if ( d < a )
+    if ( d < e )
+    cout << d << endl;
+
+    if ( e < b )
+    if ( e < c )
+    if ( e < d )
+    if ( e < a )
+    cout << e << endl;
+
+
+
+
+
+}
+
---
+++ 2.24.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    cout << " Enter an integer: " ;
+    cin >> a;
+
+    if ( a / 2 != 0 )
+        cout << " The integer is even "  << endl;
+    else
+        cout << " The integer is singular " << endl;
+        
+}
+
