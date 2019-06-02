44aeb815478d5c18c0a2accd79686710c30215cf
MSI\11346 <1134685565@qq.com>
2019-06-02 20:00:04 +0800

    homework

---
+++ 12.12/include/HourlyEmployee.h
@@ -0,0 +1,16 @@
+#ifndef HOURLYEMPLOYEE_H
+#define HOURLYEMPLOYEE_H
+
+
+class HourlyEmployee
+{
+    public:
+        HourlyEmployee();
+        virtual ~HourlyEmployee();
+
+    protected:
+
+    private:
+};
+
+#endif // HOURLYEMPLOYEE_H
